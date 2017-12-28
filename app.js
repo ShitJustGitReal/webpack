8af2f04121ec407447410b22934598b999f61cae
Obi-Bram Keno Beer <gizmo_xxl@hotmail.com>
2017-12-28 13:55:17 +0100

    optimize sourcemaps and minify javascript

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log('test');
 console.log("see who's watching");
+$('h1').append('1');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: ['./node_modules/jquery/dist/jquery.slim.min.js', './app.js', './scss/main.scss', './css/plain_css.css'],
@@ -15,6 +16,8 @@ module.exports = {
     },
     inline: true
   },
+  // opt for the most optimised form of source maps for production
+  devtool: 'cheap-module-source-map',
   module: {
     rules: [
       /*
@@ -34,6 +37,8 @@ module.exports = {
     ]
   },
   plugins: [
+    new UglifyJsPlugin(),
+    // Indicating the right environment to ensure development and test artefacts are not packed as part of the bundle
     new ExtractTextPlugin({ // define where to save the file
       filename: 'dist/[name].bundle.css',
       allChunks: true,
